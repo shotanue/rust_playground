4c55c200ec2e51652634a10b2c23ee9dc9a49053
shotanue <22065594+shotanue@users.noreply.github.com>
2019-01-31 01:29:00 +0900

    変数の勉強

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    let tmp = String::from("hogehogehoge");
+    println!("{}",tmp);
+    x + 1
+}
