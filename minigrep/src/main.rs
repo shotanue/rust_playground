63d3b05a6b075d41dcaa7833cf5a7b2a2cac7756
shotanue <22065594+shotanue@users.noreply.github.com>
2019-02-03 01:46:36 +0900

    minigrep tut

---
+++ minigrep/src/main.rs
@@ -0,0 +1,22 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+use minigrep::run;
+use minigrep::Config;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
