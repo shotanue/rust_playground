8a2ed14116271992a77cf5536934867496cacabe
shotanue <22065594+shotanue@users.noreply.github.com>
2019-02-08 00:55:12 +0900

    参照外し

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -4,6 +4,7 @@ enum List {
 }
 
 use List::{Cons, Nil};
+use std::ops::Deref;
 
 
 fn main() {
@@ -11,4 +12,26 @@ fn main() {
                     Box::new(Cons(2,
                                   Box::new(Cons(3,
                                                 Box::new(Nil))))));
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(x, 5);
+    assert_eq!(x, *y);
 }
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T>{
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+
+    fn deref(&self) -> &T{
+        &self.0
+    }
+}
+
