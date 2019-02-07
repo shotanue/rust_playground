77eac974837dbbf1ade0a63f18610ce8bef1f7d7
shotanue <22065594+shotanue@users.noreply.github.com>
2019-02-07 23:24:55 +0900

    consの実装

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,4 +1,14 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+
 fn main() {
-    let b = Box::new(5);
-    println!("{}",b);
+    let list = Cons(1,
+                    Box::new(Cons(2,
+                                  Box::new(Cons(3,
+                                                Box::new(Nil))))));
 }
