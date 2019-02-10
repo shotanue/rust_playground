2fcaf2bde017be3ae9544de752684af1d6bc1913
shotanue <22065594+shotanue@users.noreply.github.com>
2019-02-10 19:40:15 +0900

    RC のtut

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,37 +1,16 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use List::{Cons, Nil};
 use std::ops::Deref;
-
+use std::rc::Rc;
 
 fn main() {
-    let list = Cons(1,
-                    Box::new(Cons(2,
-                                  Box::new(Cons(3,
-                                                Box::new(Nil))))));
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(x, 5);
-    assert_eq!(x, *y);
+    let a = Rc::new(Cons(5,
+                 Rc::new(Cons(10,
+                               Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T>{
-        MyBox(x)
-    }
-}
-
-impl<T> Deref for MyBox<T>{
-    type Target = T;
-
-    fn deref(&self) -> &T{
-        &self.0
-    }
-}
-
