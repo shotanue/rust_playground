7ff89e7ab287b5011f94f15119e681eec81e102d
shotanue <22065594+shotanue@users.noreply.github.com>
2019-09-19 00:02:46 +0900

    lexerを途中まで

--- src/bin/calculator.rs
+++ src/bin/calculator.rs
@@ -86,3 +86,61 @@ impl LexError {
         LexError::new(LexErrorKind::Eof, loc)
     }
 }
+
+fn lex(input: &str) -> Result<Vec<Token>, LexError> {
+    let mut tokens = Vec::new();
+    let input = input.as_bytes();
+    let mut pos = 0;
+
+    macro_rules! lex_a_token {
+        ($lexer:expr) => {{
+            let (tok, p) = $lexer?;
+            tokens.push(tok);
+            pos = p;
+        }};
+    }
+
+    while pos < input.len() {
+        match input[pos] {
+            b'0'...b'9' => lex_a_token!(lex_number(input,pos)),
+            b'+' => lex_a_token!(lex_plus(input,pos)),
+            b'-' => lex_a_token!(lex_minus(input,pos)),
+            b'*' => lex_a_token!(lex_asterisk(input,pos)),
+            b'/' => lex_a_token!(lex_slash(input,pos)),
+            b'(' => lex_a_token!(lex_lparen(input,pos)),
+            b')' => lex_a_token!(lex_rparen(input,pos)),
+            b' ' | b'\n' | b'\t' => {
+                let ((), p) = skip_spaces(input, pos)?;
+                pos = p;
+            }
+            b => return Err(LexError::invalid_char(b as char, Loc(pos, pos + 1))),
+        }
+    }
+    Ok(tokens)
+}
+
+fn consume_byte(input: &[u8], pos: usize, b: u8) -> Result<(u8, usize), LexError> {
+    if input.len() <= pos {
+        return Err(LexError::eof(Loc(pos, pos)));
+    }
+
+    if input[pos] != b {
+        return Err(LexError::invalid_char(
+            input[pos] as char,
+            Loc(pos, pos + 1),
+        ));
+    }
+
+    Ok((b, pos + 1))
+}
+
+
+fn lex_plus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'+')
+        .map(|(_, end)| (Token::plus(Loc(start, end)), end))
+}
+
+
+
+
+
